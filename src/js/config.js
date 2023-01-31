0ab5ad29c934c1ea8cc770f06dce9822cf77fc19
Nikhil Satyam <110683580+Nikh9123@users.noreply.github.com>
2023-02-01 01:45:22 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10 ;
 export const RES_PER_PAGE = 10 ;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '388af28c-ef84-4865-b89b-f2c88aae1911';
 export const MODEL_CLOSE_SEC = 2.5 ;
